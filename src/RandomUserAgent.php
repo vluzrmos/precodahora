f14b9a5ef60cf4774e6f622c190bccfadd9b947f
Vagner do Carmo <vluzrmos@gmail.com>
2024-11-02 11:19:57 -0300

    Fix type

--- src/RandomUserAgent.php
+++ src/RandomUserAgent.php
@@ -4,7 +4,7 @@ namespace Vluzrmos\Precodahora;
 
 class RandomUserAgent
 {
-    protected static $userAgents = [
+    protected static array $userAgents = [
         'chrome-windows' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
         'chrome-macos' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
         'chrome-linux' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
